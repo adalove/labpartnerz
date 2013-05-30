3e357ed87a92cf755ecfdf66074cd642993eba0d
Shefali Friesen <musictechgirl@gmail.com>
2013-05-30 11:04:30 -0400

    labpartnerz app

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'rainbow'
+
+classnames = ['Adi', 'Alex', 'Anthony', 'Ben', 'Brian', 'Bryan', 'Eric', 'Jane', 'Jeff', 'Jillian', 'Joe', 'Matt', 'Nick', 'Nicky', 'Ron', 'Ryan', 'Sandip', 'Sean', 'Shefali', 'Simon', 'Stephen']
+classnames = classnames.shuffle
+
+puts `clear`
+puts "Lab Partners".color("#ffffff")
+print "What size group would you like?"
+
+group_size = gets.chomp.to_i
+number_of_groups = classnames.count / group_size
+
+puts `clear`
+number_of_groups.times do |group_number|
+  puts  "\n\n Group #{group_number} : "
+  print "#{classnames.pop(group_size).join(', ')}"
+end
+puts ", #{classnames.join(', ')}\n\n".color("#33cc00") if classnames.any?
